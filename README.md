acfb80296b8722a8b4e43e8dc4a4551d478b166c
panikkuo <nikiforpavlov00@mail.ru>
2025-03-15 20:15:12 +0300

    refactor

--- README.md
+++ README.md
@@ -1,36 +1,25 @@
-# Snake Game
+## Snake game
 
 A simple Snake game implemented in C++.
 
-## Description
+#### Description
 
 This is a classic Snake game where the player controls a snake that moves around the screen. The snake grows in length every time it eats food. The game ends when the snake runs into the walls or itself.
 
-## Installation
+#### How to play
 
-1. **Clone the repository:**
+1. Clone reposiory
+```
+git clone https://github.com/panikkuo/snake-game.git
+cd snake-game
+```
 
-    ```bash
-    git clone https://github.com/panikkuo/snakeGame.git
-    cd snakeGame
-    ```
+2. Compile the file
+```
+g++ snake.cpp -o snake
+```
 
-2. **Compile the game:**
-
-    - Using `g++` (if you have a Makefile):
-
-      ```bash
-      make
-      ```
-
-    - Alternatively, compile manually:
-
-      ```bash
-      g++ snake.cpp -o snake
-      ```
-
-3. **Run the game:**
-
-    ```bash
-    ./snake
-    ```
+1. Run the game and play
+```
+snake
+```
--- snake.cpp
+++ snake.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <time.h>
 #include <conio.h>
 #include <windows.h>
@@ -6,186 +6,202 @@
 
 using namespace std;
 
-class SnakeGame {
-private:
-	struct point {
-		int x, y;
-		bool operator == (const point& other) const {
-			return (x == other.x && y == other.y);
-		}
-		void operator = (const point& other) {
-			x = other.x;
-			y = other.y;
-		}
-		int TailType;
-	};
-
-	enum eDirection {
-		STOP = 0, LEFT, RIGHT, UP, DOWN
-	};
-	bool gameOver;
-	int fieldWidth, fieldHeight;
-	point snakeCoord, fruitCoord;
-	vector<point> tailCoords;
-	int score;
-	eDirection snakeDirection;
-	void Setup() {
-		fieldWidth = 20;
-		fieldHeight = 20;
-		gameOver = false;
-		snakeDirection = STOP;
-		snakeCoord.x = fieldWidth / 2 + 1;
-		snakeCoord.y = fieldHeight / 2 + 1;
-		fruitCoord.x = rand() % fieldWidth + 1;
-		fruitCoord.y = rand() % fieldHeight + 1;
-		score = 0;
-	}
-	bool isSnakeTail(int x, int y, int& TailType) {
-		for (const auto& tail : tailCoords) {
-			if (tail.x == x && tail.y == y) {
-				TailType = tail.TailType;
-				return true;
-			}
-		}
-		return false;
-	}
-	void Draw() {
-		system("cls"); // очистка поля
-		for (int i = 0; i < fieldWidth + 2; i++)
-			cout << "#";
-		cout << endl;
-
-		for (int y = 0; y < fieldHeight + 1; y++) {
-			for (int x = 0; x < fieldWidth + 2; x++) {
-				int TailType = -1;
-				if (x == 0 || x == fieldWidth + 1) cout << "#";
-				else if (y == snakeCoord.y && x == snakeCoord.x) cout << "0";
-				else if (y == fruitCoord.y && x == fruitCoord.x) cout << "F";
-				else if (isSnakeTail(x, y, TailType)) {
-					if (TailType == 0) cout << "o";
-					else cout << "O";
-				}
-				else cout << " ";
-			}
-			cout << endl;
-		}
-		for (int i = 0; i < fieldWidth + 2; i++)
-			cout << "#";
-		cout << endl;
-		cout << "SCORE : " << score << endl;
-	}
-	void Input() {
-		if (_kbhit()) {
-			switch (_getch())
-			{
-			case 'a':
-				if (snakeDirection != RIGHT) snakeDirection = LEFT;
-				break;
-			case 'w':
-				if (snakeDirection != DOWN) snakeDirection = UP;
-				break;
-			case 's':
-				if (snakeDirection != UP) snakeDirection = DOWN;
-				break;
-			case 'd':
-				if (snakeDirection != LEFT) snakeDirection = RIGHT;
-				break;
-			case 'x':
-				gameOver = true;
-				break;
-			}
-		}
-	}
-	bool isGameOver() {
-		bool result = false;
-		if (snakeCoord.x == 0 || snakeCoord.x == fieldWidth + 1
-			|| snakeCoord.y == -1 || snakeCoord.y == fieldHeight + 1) result = true;
-		else {
-			int countTail = tailCoords.size();
-			for (int i = 0; i < countTail; i++)
-				if (snakeCoord == tailCoords[i]) {
-					result = true;
-					break;
-				}
-		}
-		return result;
-	}
-	void Logic() {
-		point newTailCoord;
-		if (tailCoords.size() == 0) newTailCoord = snakeCoord;
-		else newTailCoord = tailCoords[tailCoords.size() - 1];
-
-		if (tailCoords.size() != 0) {
-			for (int i = tailCoords.size() - 1; i >= 1; i--) {
-				tailCoords[i] = tailCoords[i - 1];
-			}
-			tailCoords[0] = snakeCoord;
-			for (int i = tailCoords.size() - 1; i >= 1; i--) {
-				tailCoords[i].TailType = tailCoords[i - 1].TailType;
-			}
-			tailCoords[0].TailType = 0;
-			if (tailCoords.size() != 0 && tailCoords[tailCoords.size() - 1].TailType == 1) {
-				newTailCoord.TailType = 0; 
-				tailCoords.push_back(newTailCoord);
-				tailCoords[tailCoords.size() - 2].TailType = 0;
-			}
-			for (int i = tailCoords.size() - 1; i >= 1; i--) {
-				tailCoords[i].TailType = tailCoords[i - 1].TailType;
-			}
-			tailCoords[0].TailType = 0;
-		}
-
-		switch (snakeDirection)
-		{
-		case LEFT:
-			snakeCoord.x--;
-			break;
-		case RIGHT:
-			snakeCoord.x++;
-			break;
-		case UP:
-			snakeCoord.y--;
-			break;
-		case DOWN:
-			snakeCoord.y++;
-			break;
-		}
-		if (isGameOver()) gameOver = true;
-
-		if (snakeCoord == fruitCoord) {
-			score += 10;
-			fruitCoord.x = rand() % fieldWidth + 1;
-			fruitCoord.y = rand() % fieldHeight + 1;
-			if (tailCoords.size() != 0)
-				tailCoords[0].TailType = 1;
-			else {
-				newTailCoord.TailType = 0;
-				tailCoords.push_back(newTailCoord);
-			}
-		}
-		if (tailCoords.size() != 0 && tailCoords[tailCoords.size() - 1].TailType == 1) {
-			newTailCoord.TailType = 0;
-			tailCoords.push_back(newTailCoord);
-			tailCoords[tailCoords.size() - 2].TailType = 0;
-		}
-
-	}
-public:
-	void StartSnakeGame() {
-		srand(time(NULL));
-		Setup();
-		while (!gameOver) {
-			Sleep(50);
-			Draw(); Draw(); Draw();
-			Input();
-			Logic();
-		}
-		system("pause");
-	}
+struct Point {
+    int x, y;
+    bool operator == (const Point& other) {
+        return (x == other.x) && (y == other.y);
+    }
+    void operator = (const Point& other) {
+        x = other.x;
+        y = other.y;
+    }
 };
 
-int main() {
-	SnakeGame play;
-	play.StartSnakeGame();
-	return 0;
+struct Segment : Point {
+    int x, y;
+    enum Type {Head = 0, Body = 1, EatedBody = 2} segment_type;
+};
+
+struct Fruit : Point {
+    void generate(int X, int Y) {
+        x = rand() % (X - 1) + 1;
+        y = rand() % (Y - 1) + 1;
+    }
+};
+
+enum eDirection {
+    STOP, LEFT, RIGHT, UP, DOWN
+};
+
+const int fieldWidth = 21;
+const int fieldHeight = 21;
+
+bool gameOver = false;
+
+vector<Segment> snake;
+eDirection direction;
+Fruit fruit;
+int score;
+
+void Setup() {
+    Segment head;
+    head.x = fieldHeight / 2 + 1;
+    head.y = fieldHeight / 2 + 1;
+    head.segment_type = Segment::Head;
+    snake.push_back(head);
+
+    fruit.generate(fieldWidth, fieldHeight);
 }
+
+bool isSnake(int x, int y) {
+    for (int i = 0; i < snake.size(); i++) 
+        if (snake[i].x == x && snake[i].y == y)
+            return true;
+    
+    return false;
+}
+
+char getSnakeSymbol(int x, int y) {
+    for (int i = 0; i < snake.size(); i++) 
+        if (snake[i].x == x && snake[i].y == y) {
+            switch (snake[i].segment_type)
+            {
+                case Segment::Head:
+                    return '0';
+                case Segment::Body:
+                    return 'o';
+                case Segment::EatedBody:
+                    return 'O';
+            }
+        }
+    
+    return 0;
+} 
+
+void Draw() {
+    system("cls");
+
+    for (int i = 0; i <= fieldWidth; i++)
+        cout << "#";
+    cout << endl;
+
+    for (int y = 1; y <= fieldHeight - 1; y++) {
+        for (int x = 0; x <= fieldWidth; x++) {
+            if (x == 0 || x == fieldWidth) cout << "#";
+            else if (x == fruit.x && y == fruit.y) cout << "F";
+            else if (isSnake(x, y)) cout << getSnakeSymbol(x, y);
+            else cout << ' ';
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i <= fieldWidth; i++)
+        cout << "#";
+    cout << endl;
+
+    cout << "Your score : " << score;
+    cout << endl; 
+}
+
+void Input() {
+    if (_kbhit()) {
+        switch (_getch())
+        {
+        case 'w':
+            if (direction != DOWN) direction = UP;
+            break;
+        case 'a':
+            if (direction != RIGHT) direction = LEFT;
+            break;
+        case 's':
+            if (direction != UP) direction = DOWN;
+            break;
+        case 'd':
+            if (direction != LEFT) direction = RIGHT;
+            break;
+        case 'x':
+            gameOver = true;
+            break;
+        }
+    }
+}
+
+bool isGameOver() {
+    if (snake[0].x == 0 || snake[0].x == fieldWidth ||
+        snake[0].y == 0 || snake[0].y == fieldHeight) {
+            cout << snake[0].y;
+            return true;
+
+        }
+        
+    
+    for (int i = 1; i < snake.size(); i++)
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y)
+            return true;
+    
+    return false;
+}
+
+void Logic() {
+    Segment newSegment;
+    newSegment.x = snake[snake.size() - 1].x;
+    newSegment.y = snake[snake.size() - 1].y;
+    newSegment.segment_type = Segment::Body;
+
+    snake[0].segment_type = Segment::Body;
+    for (int i = snake.size() - 1; i > 0; i--) {
+        snake[i].x = snake[i - 1].x;
+        snake[i].y = snake[i - 1].y;
+        snake[i].segment_type = snake[i - 1].segment_type;
+    }
+    
+    snake[0].segment_type = Segment::Head;
+    
+    switch (direction)
+	{
+		case LEFT:
+            snake[0].x--;
+			break;
+		case RIGHT:
+            snake[0].x++;
+			break;
+		case UP:
+            snake[0].y--;
+			break;
+		case DOWN:
+			snake[0].y++;
+    }
+
+    if (isGameOver()) gameOver = true;
+
+    if (snake[0].x == fruit.x && snake[0].y == fruit.y) {
+        score += 100;
+        if (snake.size() == 1)
+            snake.push_back(newSegment);
+        else 
+            snake[1].segment_type = Segment::EatedBody;
+        fruit.generate(fieldWidth, fieldHeight);
+    }
+
+    if (snake[snake.size() - 1].segment_type == Segment::EatedBody) {
+        snake[snake.size() - 1].segment_type = Segment::Body;
+        snake.push_back(newSegment);
+    }
+}
+
+int main() {
+    srand(time(NULL));
+    Setup();
+    while (!gameOver) {
+        Sleep(150);
+        Draw();
+        Input();
+        Logic();
+    }
+    system("pause");
+}
+
+
+//field
+//0----21
+//#1-20#
